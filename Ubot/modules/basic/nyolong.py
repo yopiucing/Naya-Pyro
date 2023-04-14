bce318a9abdb2b29a2ec38ff4a3d0ac498dace71
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-14 08:14:11 +0700

    update nyolong.py

--- Ubot/modules/basic/nyolong.py
+++ Ubot/modules/basic/nyolong.py
@@ -1,46 +1,128 @@
-# Credits Thx Tomi Setiawan
-# Copas Teriak Copas MONYET
-# Gay Teriak Gay Anjeng
-# @Rizzvbss | @Kenapanan
-# Kok Bacot
-# © @KynanSupport
-# FULL MONGO NIH JING FIX MULTI CLIENT
+# Copyright (C) 2020-2021 by Toni880@Github, < https://github.com/Toni880 >.
+#
+# This file is part of < https://github.com/Toni880/Prime-Userbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Toni880/Prime-Userbot/blob/master/LICENSE >
+# kenkan
+# abdul
+# recode dikit #amang
+# All rights reserved.
 
-import asyncio
+import os
+
+from pyrogram import *
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.errors import RPCError
+from pyrogram.types import *
 
-from . import *
 from ubotlibs.ubot.utils import get_arg
+from . import *
 
-
-
-@Client.on_message(filters.command(["curi"], "") & filters.me)
-async def copy_msg(client: Client, message: Message):
-    Tm = await message.reply("`Processing...`")
+@Ubot("copy", "")
+async def nyolongnih(client, message):
+    await message.edit("Processing...")
     link = get_arg(message)
-    if not link:
-        return await Tm.edit(f"<b><code>{message.text}</code> [link_konten_telegram]</b>")
-    if link.startswith(("https", "t.me")):
-        msg_id = int(link.split("/")[-1])
-        if "t.me/c/" in link:
-            chat = int("-100" + str(link.split("/")[-2]))
-        else:
-            chat = str(link.split("/")[-2])
+    msg_id = int(link.split("/")[-1])
+    if "t.me/c/" in link:
         try:
-            get = await client.get_messages(chat, msg_id)
-            await get.copy(message.chat.id)
-            return await Tm.delete()
-        except Exception as error:
-            await Tm.edit(error)
+            chat = int("-100" + str(link.split("/")[-2]))
+            dia = await client.get_messages(chat, msg_id)
+        except RPCError:
+            await message.edit("Sepertinya terjadi kesalahan")
     else:
-        await Tm.edit("`Berikan link yang valid.`")
+        try:
+            chat = str(link.split("/")[-2])
+            dia = await client.get_messages(chat, msg_id)
+        except RPCError:
+            await message.edit("Sepertinya terjadi kesalahan")
+    anjing = dia.caption or None
+    if dia.text:
+        await dia.copy(message.chat.id)
+        await message.delete()
+    if dia.photo:
+        anu = await client.download_media(dia)
+        await client.send_photo(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.video:
+        anu = await client.download_media(dia)
+        await client.send_video(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.audio:
+        anu = await client.download_media(dia)
+        await client.send_audio(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.voice:
+        anu = await client.download_media(dia)
+        await client.send_voice(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.document:
+        anu = await client.download_media(dia)
+        await client.send_document(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+    else:
+        await message.edit("Sepertinya terjadi kesalahan")
+
+
+@Ubot("curi", "")
+async def pencuri(client, message):
+    dia = message.reply_to_message
+    if not dia:
+        await message.edit("Mohon balas ke media")
+    anjing = dia.caption or None
+    await message.edit("Processing...")
+    if dia.text:
+        await dia.copy(message.chat.id)
+        await message.delete()
+    if dia.photo:
+        anu = await client.download_media(dia)
+        await client.send_photo(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.video:
+        anu = await client.download_media(dia)
+        await client.send_video(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.audio:
+        anu = await client.download_media(dia)
+        await client.send_audio(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.voice:
+        anu = await client.download_media(dia)
+        await client.send_voice(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+
+    if dia.document:
+        anu = await client.download_media(dia)
+        await client.send_document(message.chat.id, anu, anjing)
+        await message.delete()
+        os.remove(anu)
+    else:
+        await message.edit("Sepertinya terjadi kesalahan")
 
 
 
 add_command_help(
     "nyolong",
     [
-        [f"curi", "nyolong konten orang"],
+        [
+            "copy <link protected channel.>",
+            "Clone restricted media."],
+        [   "curi <reply message>",
+            "Clone from the protected media or timer message."],
     ],
 )
