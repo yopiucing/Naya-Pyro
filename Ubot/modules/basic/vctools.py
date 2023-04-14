2c51ff316eea33af6ecc55e29b266f014a7b2fda
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-15 00:51:05 +0700

    update vctools.py

--- Ubot/modules/basic/vctools.py
+++ Ubot/modules/basic/vctools.py
@@ -27,12 +27,10 @@ async def get_group_call(
     chat_peer = await client.resolve_peer(message.chat.id)
     if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
-            full_chat = (
-                await client.invoke(GetFullChannel(channel=chat_peer))
-            ).full_chat
+            full_chat = (await client.send(GetFullChannel(channel=chat_peer))).full_chat
         elif isinstance(chat_peer, InputPeerChat):
             full_chat = (
-                await client.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+                await client.send(GetFullChat(chat_id=chat_peer.chat_id))
             ).full_chat
         if full_chat is not None:
             return full_chat.call
